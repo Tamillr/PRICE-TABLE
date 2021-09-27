6f1b8b4e67ea1dd4049c833dfb70374affe81c4a
Tamizh <89194597+Tamillr@users.noreply.github.com>
2021-09-27 14:37:22 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,10 @@
+const head = {
+
+    name: "DATA HOST",
+
+    text: "Our cost-effective web hosting is suitable for establishing a Web presence or a website that requires the deployment and maintenance of fully integrated e-business solutions.",
+
+}
+
+export default head;
+
---
+++ src/datapricingtable.js
@@ -0,0 +1,98 @@
+const cloudPlans = [
+    {
+
+        plan: 'FREE',
+
+        price: 'FREE',
+
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        storage: '1 TB Cloud Space',
+
+        dataTransfer: '5 TB Data Transfer',
+
+        panel: 'Fully Managed Panel',
+
+        support: 'NULL',
+
+        themes: 'NULL',
+
+        others: 'NULL',
+
+        tag: 'NULL'
+
+    }, {
+
+        plan: 'BASIC',
+
+        price: '$15.50 MONTHLY',
+
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        storage: '500 GB Storage Space',
+
+        dataTransfer: '3 TB Data Transfer',
+
+        panel: 'Basic Managed Panel',
+
+        support: '24/7 Fast Support',
+
+        themes: '100 Premium Themes',
+
+        others: 'Cancel or Upgrade Anytime',
+
+        tag: 'SELECT PLAN'
+
+    },
+
+    {
+
+        plan: 'INTERMEDIATE',
+
+        price: '$30.00 MONTHLY',
+
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        storage: '1 TB Cloud Space',
+
+        dataTransfer: '8 TB Data Transfer',
+
+        panel: 'Fully Managed Panel',
+
+        support: '15-minute Quick Support',
+
+        themes: 'Unlimited Web Addons',
+
+        others: 'Cancel or Upgrade Anytime',
+
+        tag: 'SELECT PLAN'
+
+    },
+
+    {
+
+        plan: 'ADVANCED',
+
+        price: '$72.25 MONTHLY',
+
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+
+        storage: '4 TB Cloud Space',
+
+        dataTransfer: '20 TB Data Transfer',
+
+        panel: 'Fully Managed Panel',
+
+        support: '15-minute Quick Support',
+
+        themes: 'Top Notch Web Apps',
+
+        others: 'Advanced Scalable',
+
+        tag: 'TAKE IT NOW'
+
+    }
+
+];
+
+export default cloudPlans;
